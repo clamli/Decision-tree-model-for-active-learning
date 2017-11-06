c300732b2e064f2675ecd3de2e66274ffb9a78fb
clamli <boyizjuer2017@gmail.com>
2017-11-06 12:16:31 -0800

    update fdt_process, add dump and load

--- PyCharm Project/fdt_process.py
+++ PyCharm Project/fdt_process.py
@@ -2,6 +2,7 @@
 from scipy.sparse import load_npz
 import dt_model as dt
 import tool_function as tf
+import klepto
 ############################################
 
 ############### Load Data ##################
@@ -11,34 +12,30 @@ print("file load DONE")
 ############################################
 
 ############### Build Tree #################
-''' Save to file 'tree.pkl' '''
 start = 0
 end = int(rating_matrix_csc.shape[1] * 0.75)
-endt = int(rating_matrix_csc.shape[1] * 0.6)
 dtmodel_realdata = dt.DecisionTreeModel(rating_matrix_csc[:, start:end], depth_threshold = 10)
 dtmodel_realdata.build_model()
-o_tree = open('./treeFile/tree.pkl', 'wb')
-pickle.dump(dtmodel_realdata, o_tree)
-o_tree.close()
+Tree = klepto.archives.dir_archive('treeFile', cached=True, serialized=True)
+Tree['lr_bound'] = dtmodel_realdata.lr_bound
+Tree['tree'] = dtmodel_realdata.tree
+Tree['split_item'] = dtmodel_realdata.split_item
+Tree['rI'] = dtmodel_realdata.rI
+Tree['rU'] = dtmodel_realdata.rU
+Tree.dump()
+Tree.clear()
 ############################################
 
 ######################## Build Predict Model #########################
-plambda_candidates = {}
-inputf = open('./treeFile/tree.pkl', 'rb')
-dtmodel_realdata = pickle.load(inputf)
-
-#### rI, rU
-#### lr_bound
-#### split_item
-#### tree
-
+Tree = klepto.archives.dir_archive('treeFile', cached=True, serialized=True)
+Tree.load('treeFile')
 for level in lr_bound:
 	plambda_candidates[level] = list(np.arange(0.001, 0.05, 0.0005))
-prediction_model = tf.generate_prediction_model(lr_bound, tree, rI, rU, plambda_candidates, rating_matrix_csc[:, endt:end])
+prediction_model = tf.generate_prediction_model(Tree['lr_bound'], Tree['tree'], Tree['rI'], Tree['rU'], plambda_candidates, rating_matrix_val_csc)
 ######################################################################
 
 ######################### Test for New-user ##########################
-rmse_result = tf.pred_RMSE_for_new_user(split_item, rI, prediction_model, rating_matrix_csc[:, end:])
+rmse_result = tf.pred_RMSE_for_new_user(Tree['split_item'], rI, prediction_model, rating_matrix_csc[:, end:])
 ######################################################################
 
 
